4955e0f7c4af4d665a1d2e63e4a3d8deabb14906
francorivaas <65259655+francorivaas@users.noreply.github.com>
2022-04-29 13:47:56 -0300

    spear and gun working propperly

--- Assets/_Main/Scripts/Weapons/Spear.cs
+++ Assets/_Main/Scripts/Weapons/Spear.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Spear : WeaponActor
 {
+    [SerializeField] private float spearDamage;
+    [SerializeField] private GameObject damageArea;
+
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -14,10 +17,39 @@ public class Spear : WeaponActor
     protected override void Update()
     {
         base.Update();
+        ActivateDamageArea();
     }
 
     public override void Attack()
     {
         base.Attack();
+    }
+
+    private void ActivateDamageArea()
+    {
+        if (damageArea != null)
+        {
+            if (canAttack)
+                damageArea.SetActive(true);
+
+            else
+                damageArea.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        #region Get Actor Data
+
+        HealthController actorHealth = other.gameObject.GetComponent<HealthController>();
+        GameObject actorLayerObj = other.gameObject.transform.gameObject;
+
+        #endregion Get Actor Data
+
+        if (actorHealth != null && actorLayerObj.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            actorHealth.TakeDamage(spearDamage);
+            Debug.Log($"La vida de {actorHealth.name} es de {actorHealth.CurrentHealth}");
+        }
     }
 }
